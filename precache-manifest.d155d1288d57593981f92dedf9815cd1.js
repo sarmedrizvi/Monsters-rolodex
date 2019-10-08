3efb3b9d9dd9bffcca641b874e84846027f3299f
sarmed Rizvi <sarmedrizvi@gmail.com>
2019-10-08 23:31:46 +0500

    Updates

---
+++ precache-manifest.d155d1288d57593981f92dedf9815cd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef75e4a7642775711bfc47ec8a8b7cf",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9f759aa7a3432093e435",
+    "url": "/Monsters-rolodex/static/css/main.94f79cd3.chunk.css"
+  },
+  {
+    "revision": "1f392377f23e9ae362a7",
+    "url": "/Monsters-rolodex/static/js/2.7f73eb55.chunk.js"
+  },
+  {
+    "revision": "9f759aa7a3432093e435",
+    "url": "/Monsters-rolodex/static/js/main.d9c068c5.chunk.js"
+  },
+  {
+    "revision": "f965595906efb1da3bad",
+    "url": "/Monsters-rolodex/static/js/runtime-main.d93fa5d6.js"
+  }
+]);
