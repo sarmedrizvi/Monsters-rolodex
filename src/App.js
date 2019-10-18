53359d24f25e9f56f6c892dab5bcf44e52d60d94
sarmed Rizvi <sarmedrizvi@gmail.com>
2019-10-18 15:19:41 +0500

    Animation AOS

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
       SearchField:''
     }
   }
+  
   componentDidMount()
   {
     fetch("https://jsonplaceholder.typicode.com/users")
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Card-css.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-
+AOS.init();
 export const Card = (props) =>(
-  
-      <div className="card">
+      
+      <div className="card" data-aos="fade-up">
       <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="monsters"></img>
          <h2 >{props.monster.name}</h2>
          <h4 >{props.monster.email}</h4>
